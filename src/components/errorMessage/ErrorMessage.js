9a0d5e8880f346b64ba4ad26a7aa2b8b1e88d916
Igor Zharov <stawr.900@gmail.com>
2022-08-09 11:45:37 +0300

    -aadd_api_charlist

--- src/components/errorMessage/ErrorMessage.js
+++ src/components/errorMessage/ErrorMessage.js
@@ -3,7 +3,7 @@ import img from './error.gif';
 const ErrorMessage = () => { //картинка, при возврате ошибки с сервера
     return (
         // <img src={process.env.PUBLIC_URL + '/error.gif'} /> - обращаемся к окружению и находим папку public
-        <img style={{ display: 'block', width: "250px", height: "250px",objectFit: 'contain', margin: "0 auto"}}  src={img} alt="Error"/>
+        <img style={{ display: 'block', width: "250px", height: "250px",objectFit: 'contain', margin: "0 auto"}} src={img} alt="Error"/>
     )
 }
 
--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -7,11 +7,6 @@ import './randomChar.scss';
 import mjolnir from '../../resources/img/mjolnir.png';
 
 class RandomChar extends Component {
-    constructor(props) {
-        super(props);
-        this.updateChar(); //вызываем заранее, перед этапом создания объекта с персонажем
-    }
-
     state = { //поля классов
         char: {}, //объект с персонажами
         loading: true, //при загрузке показ спиннера
@@ -20,10 +15,25 @@ class RandomChar extends Component {
 
     marvelService = new MarvelService();
 
-    onChatLoaded = (char) => { //записываем в объект с персонажами, после загрузки из API
+    componentDidMount() { //при создании элемента на странице
+        this.updateChar();
+        // this.timerId = setInterval(this.updateChar, 3000);
+    }
+
+    componentWillUnmount() { //при удалении элемента со страницы
+        clearInterval(this.timerId);
+    }
+
+    onCharLoaded = (char) => { //записываем в объект с персонажами, после загрузки из API
         this.setState({
             char, 
             loading: false //загрузка выключается, после подгрузки всех характеристик персонажа
+        })
+    }
+
+    onCharLoading = () => {
+        this.setState({
+            loading: true
         })
     }
 
@@ -36,9 +46,10 @@ class RandomChar extends Component {
 
     updateChar = () => {
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000); //рандомное число
+        this.onCharLoading();
         this.marvelService
             .getCharacter(id)
-            .then(this.onChatLoaded) //перезаписываем объект с нужными характеристиками из API
+            .then(this.onCharLoaded) //перезаписываем объект с нужными характеристиками из API
             .catch(this.onError);
     }
 
@@ -61,7 +72,7 @@ class RandomChar extends Component {
                     <p className="randomchar__title">
                         Or choose another one
                     </p>
-                    <button className="button button__main">
+                    <button className="button button__main" onClick={this.updateChar}>
                         <div className="inner">try it</div>
                     </button>
                     <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
@@ -73,10 +84,14 @@ class RandomChar extends Component {
 
 const View = ({char}) => {
     const {name, description, thumbnail, homepage, wiki} = char;
+    let imgStyle = {'objectFit' : 'cover'};
+    if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+        imgStyle = {'objectFit' : 'contain'};
+    }
 
     return (
         <div className="randomchar__block">
-            <img src={thumbnail} alt="Random character" className="randomchar__img"/>
+            <img src={thumbnail} alt="Random character" className="randomchar__img" style={imgStyle}/>
             <div className="randomchar__info">
                 <p className="randomchar__name">{name}</p>
                 <p className="randomchar__descr">

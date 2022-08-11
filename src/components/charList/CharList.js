44312027cf651a28883e5be59fa10da5a7579d05
Igor Zharov <stawr.900@gmail.com>
2022-08-11 20:48:42 +0300

    charinfo_skeleton_udaptive

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -44,9 +44,10 @@ class CharList extends Component {
             }
             
             return (
-                <li 
+                <li //id передается выше в app
                     className="char__item"
-                    key={item.id}>
+                    key={item.id}
+                    onClick={() => this.props.onCharSelected(item.id)}>
                         <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
                         <div className="char__name">{item.name}</div>
                 </li>

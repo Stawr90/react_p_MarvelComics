e81109e3e6a9f4abb5d64edf85f99261fb874d34
Igor <stawr.900@gmail.com>
2022-11-03 13:44:42 +0300

    add_hook_useHttp

---
+++ src/hooks/http.hook.js
@@ -0,0 +1,30 @@
+import {useState, useCallback} from 'react';
+
+export const useHttp = () => { // Хук запроса и обработки данных с сервера
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+
+    const request = useCallback(async (url, method='GET', body = null, headers = {'Content-type': 'applicatiion/json'}) => {
+        setLoading(true);
+
+        try {
+            const response = await fetch(url, {method, body, headers});
+
+            if (!response.ok) {
+                throw new Error(`Could not fetch ${url}, status: ${res.status}`); //отправим в catch
+            }
+
+            const data = await response.json();
+            setLoading(false); //как данные пришли, выключим загрузку
+            return data;
+        } catch(e) {
+            setLoading(false);
+            setError(e.message); //поменяем на заранее подготовленное сообщение (установим ошибку)
+            throw(e); //выкидываем ошибку
+        }
+    }, []);
+
+    const clearError = useCallback(() => setError(null), []);
+
+    return {loading, request, error, clearError};
+}
